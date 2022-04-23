982bbc9d7631f17c4bcdf0f0f7d227c7ff117bec
wikiwikitang <ctang13@binghamton.edu>
2022-04-22 17:12:36 -0700

    my first github commit

---
+++ demo.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('test-id');
+btn.onclick = () => {
+  debugger;
+  alert('This is a Test');
+};
+
+// btn.addEventListener('click', () => {
+//   alert('This is a Test');
+// });
